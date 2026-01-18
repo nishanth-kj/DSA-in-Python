aa42b0beb1cc46a5b687b941de393b61e023ad03
nishanthkj <nishanthkj12@gmail.com>
2026-01-18 14:48:26 +0530

    hashing

--- Code/hashing.py
+++ Code/hashing.py
@@ -1,23 +1,43 @@
 class Hashing:
-    def __init__(self,arr):
+    def __init__(self, arr):
         self.arr = arr
-    
-    
+        self.freq = {}
+        self.max_freq = float('-inf')
+        self.min_freq = float('inf')
+        self.max_key = None
+        self.min_key = None
+
+        self.frequency()
+        self.high_frequency_and_lowest_frequency()
+
     def frequency(self):
-        freq = {}
         for i in self.arr:
-            if i not in freq:
-                freq[i] = 1
+            if i not in self.freq:
+                self.freq[i] = 1
             else:
-                freq[i] = freq[i] + 1
-        print(freq)
-    
-if __name__=="__main__":
+                self.freq[i] += 1
+
+    def high_frequency_and_lowest_frequency(self):
+        for key, value in self.freq.items():
+            if value > self.max_freq:
+                self.max_freq = value
+                self.max_key = key
+
+            if value < self.min_freq:
+                self.min_freq = value
+                self.min_key = key
+
+
+if __name__ == "__main__":
     arr = []
-    n = int(input("Enter the Size of the list"))
-    for i in range(n):
-        val = None
-        val = input("Enter the value")
-        arr.append(val)
+    n = int(input("Enter the size of the list: "))
+    for _ in range(n):
+        arr.append(input("Enter the value: "))
+
     hashing = Hashing(arr)
-    hashing.frequency()
+
+    print(f"Frequency Map       : {hashing.freq}")
+    print(f"Lowest Frequency    : {hashing.min_freq}")
+    print(f"Lowest Freq Element : {hashing.min_key}")
+    print(f"Highest Frequency   : {hashing.max_freq}")
+    print(f"Highest Freq Element: {hashing.max_key}")
